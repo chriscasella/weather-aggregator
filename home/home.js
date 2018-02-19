f538c0a8e5eb9bb7b3ec7f53f819b75d08a3036d
chriscasella <chris.casella@gmail.com>
2018-02-18 22:53:13 -0500

    changed easing times made var

--- home/home.js
+++ home/home.js
@@ -4,29 +4,34 @@ app.controller('HomeController', ['$scope', '$rootScope', function ($scope, $roo
     };
     $rootScope.zip = null;
 
+    // $scope.consoleThis = function(){
+    //     console.log($rootScope.zip);
+    // };
+
     $scope.animations = function(){
+        var easeTime = [.91, -0.54, .29, 1.56];
         this.headerAnime = anime({
             targets: ".zip-header",
-            easing: 'easeInOutQuart',
+            easing: easeTime,
             translateY: 250,
             opacity: 1
         });
 
         this.labelAnime = anime({
             targets: "#zip-label",
-            easing: 'easeInOutQuart',
+            easing: easeTime,
             opacity: 1
         });
 
         this.zipInputAnime = anime({
             targets: '.zip-input',
-            easing: 'easeInOutQuart',
+            easing: easeTime,
             opacity: 1
         });
 
         this.zipSubmitAnime = anime({
             targets: '#zip-submit',
-            easing: 'easeInOutQuart',
+            easing: easeTime,
             opacity: 1
         });
     };
